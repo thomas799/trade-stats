8569d3d7156b949c9b6f6a42156227995e7b1f1a
Temich7799 <artemgl9999@gmail.com>
2025-12-11 16:07:49 +0200

    fix: update ESLint configuration and rename lint fix script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,123 +1,127 @@
-import eslint from "@eslint/js";
-import perfectionistPlugin from "eslint-plugin-perfectionist";
-import prettierRecommendedPlugin from "eslint-plugin-prettier/recommended";
-import hooksPlugin from "eslint-plugin-react-hooks";
-import reactPlugin from "eslint-plugin-react";
-import globals from "globals";
+import eslint from '@eslint/js';
+import perfectionistPlugin from 'eslint-plugin-perfectionist';
+import prettierRecommendedPlugin from 'eslint-plugin-prettier/recommended';
+import reactPlugin from 'eslint-plugin-react';
+import hooksPlugin from 'eslint-plugin-react-hooks';
+import globals from 'globals';
 
-export default eslint.config(
+export default [
   eslint.configs.recommended,
-  reactPlugin.configs.flat["jsx-runtime"],
+  reactPlugin.configs.flat['jsx-runtime'],
   prettierRecommendedPlugin,
-  { ignores: ["**/build/**", "**/dist/**", "**/node_modules/**", "**/.next/**"] },
   {
-    files: ["**/*.{js,jsx,mjs,cjs}"],
+    ignores: ['**/build/**', '**/dist/**', '**/node_modules/**', '**/.next/**']
+  },
+  {
+    files: ['**/*.{js,jsx,mjs,cjs}'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
       globals: {
-        JSX: "writable",
-        React: "writable",
+        JSX: 'writable',
+        React: 'writable',
         ...Object.fromEntries(
           Object.entries(globals.browser).map(([key, value]) => [
             key.trim(),
-            value,
-          ]),
+            value
+          ])
         ),
         ...Object.fromEntries(
           Object.entries(globals.node).map(([key, value]) => [
             key.trim(),
-            value,
-          ]),
-        ),
+            value
+          ])
+        )
       },
       parserOptions: {
         ecmaFeatures: {
-          jsx: true,
+          jsx: true
         },
-        sourceType: "module",
+        sourceType: 'module'
       },
+      sourceType: 'module'
     },
     plugins: {
       perfectionist: perfectionistPlugin,
-      "react-hooks": hooksPlugin,
       react: reactPlugin,
+      'react-hooks': hooksPlugin
     },
     rules: {
-      "no-unused-vars": [
-        "warn",
+      'no-unused-vars': [
+        'warn',
         {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
+          argsIgnorePattern: '^_',
+          caughtErrors: 'none',
+          varsIgnorePattern: '^_'
+        }
       ],
-      "perfectionist/sort-imports": [
-        "error",
+      'perfectionist/sort-imports': [
+        'error',
         {
           customGroups: {
             value: {
-              "custom-assets": ["^assets/"],
-              "custom-common": ["^common/"],
-              react: ["^react$", "^react-dom$", "^react-router-dom$"],
-            },
+              'custom-assets': ['^assets/'],
+              'custom-common': ['^common/'],
+              react: ['^react$', '^react-dom$', '^react-router-dom$']
+            }
           },
           groups: [
-            ["react"],
-            ["builtin", "external"],
-            ["internal", "internal-type"],
-            ["custom-common"],
+            ['react'],
+            ['builtin', 'external'],
+            ['internal', 'internal-type'],
+            ['custom-common'],
             [
-              "parent-type",
-              "sibling-type",
-              "index-type",
-              "parent",
-              "sibling",
-              "index",
+              'parent-type',
+              'sibling-type',
+              'index-type',
+              'parent',
+              'sibling',
+              'index'
             ],
-            "object",
-            "unknown",
-            "custom-assets",
+            'object',
+            'unknown',
+            'custom-assets'
           ],
-          partitionByNewLine: false,
-        },
+          partitionByNewLine: false
+        }
       ],
-      "perfectionist/sort-named-imports": [
-        "error",
+      'perfectionist/sort-named-imports': [
+        'error',
         {
-          groupKind: "values-first",
-        },
+          groupKind: 'values-first'
+        }
       ],
-      "perfectionist/sort-objects": ["error"],
-      "prettier/prettier": [
-        "error",
+      'perfectionist/sort-objects': ['error'],
+      'prettier/prettier': [
+        'error',
         {
-          parser: "babel",
+          parser: 'babel',
           singleQuote: true,
-          trailingComma: "none",
-        },
+          trailingComma: 'none'
+        }
       ],
-      "react-hooks/exhaustive-deps": "off",
-      "react-hooks/rules-of-hooks": "warn",
-      "react/jsx-boolean-value": ["error", "never"],
-      "react/jsx-no-useless-fragment": "error",
-      "react/jsx-sort-props": [
-        "error",
+      'react-hooks/exhaustive-deps': 'off',
+      'react-hooks/rules-of-hooks': 'warn',
+      'react/jsx-boolean-value': ['error', 'never'],
+      'react/jsx-no-useless-fragment': 'error',
+      'react/jsx-sort-props': [
+        'error',
         {
           callbacksLast: true,
           ignoreCase: true,
           reservedFirst: true,
-          shorthandFirst: true,
-        },
+          shorthandFirst: true
+        }
       ],
-      "react/prop-types": "off",
+      'react/jsx-uses-vars': 'warn',
+      'react/prop-types': 'off'
     },
     settings: {
       perfectionist: {
-        order: "asc",
+        order: 'asc',
         partitionByComment: true,
         partitionByNewLine: true,
-        type: "natural",
-      },
-    },
-  },
-);
+        type: 'natural'
+      }
+    }
+  }
+];
