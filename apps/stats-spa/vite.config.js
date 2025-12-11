ed157453e5d5755b480a004962e6f975f7ee4d64
Temich7799 <artemgl9999@gmail.com>
2025-12-12 00:01:51 +0200

    feat: add node polyfills and enhance server configuration in Vite setup

--- apps/stats-spa/vite.config.js
+++ apps/stats-spa/vite.config.js
@@ -1,7 +1,35 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true
+      },
+      include: ['buffer', 'process', 'util']
+    })
+  ],
+  server: {
+    hmr: {
+      host: 'localhost',
+      port: 5173
+    },
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api': {
+        changeOrigin: true,
+        logLevel: 'debug',
+        secure: false,
+        target: process.env.VITE_API_URL || 'http://172.18.0.4',
+        ws: true
+      }
+    }
+  }
 });
